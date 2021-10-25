74e8afba8f2a18132133cb20cb00789d39066743
Tyoma <risendrums777@gmail.com>
2021-10-25 18:14:49 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const imgMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+  })
+  .join('');
+
+galleryRef.insertAdjacentHTML('beforeend', imgMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+const onModalOpen = e => {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  } else {
+    lightbox.open();
+  }
+};
+galleryRef.addEventListener('click', onModalOpen);
