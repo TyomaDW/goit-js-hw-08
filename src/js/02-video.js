d63770d54cb13323e330edccdef8f1f56761ea42
Tyoma <risendrums777@gmail.com>
2021-10-26 00:04:30 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+const throttle = require('lodash.throttle');
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+const TIME_KEY = 'videoplayer-current-time';
 
+player.setCurrentTime(localStorage.getItem(TIME_KEY));
+
+const onPlay = () => {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      localStorage.setItem(TIME_KEY, seconds);
+    })
+    .catch(function (error) {
+      console.log(error.name);
+    });
+};
+
+const lodashThrottle = throttle(onPlay, 1000);
+
+player.on('play', lodashThrottle);
