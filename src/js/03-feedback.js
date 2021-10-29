5101527c28150eb96be6193c98e5b9389db30059
Tyoma <risendrums777@gmail.com>
2021-10-30 00:21:22 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+const emailRef = document.querySelector('input[name="email"]');
+const messageRef = document.querySelector('textarea[name="message"]');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+formRef.addEventListener('submit', onFormSubmit);
+formRef.addEventListener('input', throttle(onInputMessage, 500));
+
+checkSavedImputs();
+
+function onInputMessage(event) {
+  formData[event.target.name] = event.target.value.trim();
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+  event.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+function checkSavedImputs() {
+  const saveInputs = localStorage.getItem(LOCALSTORAGE_KEY);
+
+  if (saveInputs) {
+    const { email, message } = JSON.parse(saveInputs);
+    messageRef.value = message;
+    emailRef.value = email;
+  }
+}
